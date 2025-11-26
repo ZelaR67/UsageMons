45738cd050dae0f38424f8b1464c855bbd661b26
nKonnuru <112659605+nKonnuru@users.noreply.github.com>
2025-11-26 13:38:47 -0600

    fix structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <MetadataProvider>
       <RatingProvider>
-        <Router>
+        <Router basename={import.meta.env.BASE_URL}>
           <div className="fixed-bg" />
           <div className="min-h-screen text-gray-800 flex flex-col font-sans relative">
             <Routes>
